4537429fe7b01fee2cb7539b267cf890894cc84a
wujian016 <wujian016@163.com>
2013-07-15 07:52:34 +0800

    my hand commit move newfiles to root6

---
+++ ConsoleApplication2/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Class2
+    {
+        string Name = "";
+        public Class2()
+        {
+            Name = "mane";
+        }
+
+        public Class2(string name)
+        {
+            Name = name;
+        }
+    }
+}
